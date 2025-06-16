2417c2d389676926f01bbdc2081d2724d63c44ba
widse <wngud6012@naver.com>
2025-06-16 22:03:48 +0900

    chore(@web): add tanstack-query

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import RQProvider from "./shared/provider/RQProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <RQProvider>{children}</RQProvider>
+      </body>
     </html>
   );
 }
