0a84703565a8461925956ce483eb8872b10aeefc
seungdeok <akk9132@naver.com>
2025-06-20 15:18:11 +0900

    chore(auth-api):hot-module-replacement 적용

--- apps/auth-api/src/main.ts
+++ apps/auth-api/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
+// https://docs.nestjs.com/recipes/hot-reload#hot-module-replacement
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
