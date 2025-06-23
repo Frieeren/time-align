b2c75e319b2999a08db1df366b6fb90b2b86d35e
seungdeok <akk9132@naver.com>
2025-06-23 15:31:33 +0900

    docs(auth-api): update swagger dto scheme

--- apps/auth-api/src/auth/dto/oauth-login.dto.ts
+++ apps/auth-api/src/auth/dto/oauth-login.dto.ts
@@ -1,22 +1,28 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, IsNotEmpty, IsOptional } from "class-validator";
 
 export class OAuthLoginDto {
+  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   email!: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name!: string;
 
+  @ApiProperty()
   @IsString()
-  @IsOptional()
+  @IsNotEmpty()
   image!: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   provider!: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   providerId!: string;
