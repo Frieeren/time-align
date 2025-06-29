21c67122f8f30f561c684fbd3bf2e14476a388e7
widse <wngud6012@naver.com>
2025-06-29 23:19:10 +0900

    fix(@web): add test login page

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -1,26 +1,10 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import GoogleButton from "./_components/GoogleButton";
 import { useSession } from "next-auth/react";
-import { httpClient } from "../../shared/api/http";
 
 export default function TestAuthPage() {
   const { data: session } = useSession();
-
-  const [profile, setProfile] = useState(null);
-
-  useEffect(() => {
-    const getProfile = async () => {
-      const response = await httpClient({
-        url: "/auth/profile",
-      });
-
-      console.log(response);
-    };
-
-    getProfile();
-  }, []);
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
---
+++ apps/web/app/profile/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { httpClient } from "../../shared/api/http";
+
+export default function TestAuthPage() {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    const getProfile = async () => {
+      const response = await httpClient({
+        url: "/auth/profile",
+      });
+
+      console.log(response);
+    };
+
+    getProfile();
+  }, []);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ width: 300, marginTop: 20 }}>
+        <pre style={{ 
+          whiteSpace: 'pre-wrap', 
+          wordWrap: 'break-word',
+          overflowWrap: 'break-word',
+          maxWidth: '100%'
+        }}>
+          {JSON.stringify(session, null, 2)}
+        </pre>
+      </div>
+    </div>
+  );
+}
