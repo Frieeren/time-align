b92087c3a61df0cedea9400ea14bbd84d3f9bd42
seungdeok <akk9132@naver.com>
2025-07-03 20:17:08 +0900

    chore(api): db Entity 작성

---
+++ apps/api/src/enums/schedule.enum.ts
@@ -0,0 +1,11 @@
+export enum ScheduleParticipantRole {
+  ORGANIZER = "ORGANIZER",
+  ATTENDEE = "ATTENDEE",
+}
+
+export enum ScheduleParticipantResponseStatus {
+  PENDING = "PENDING",
+  ACCEPTED = "ACCEPTED",
+  DECLINED = "DECLINED",
+  TENTATIVE = "TENTATIVE",
+}
