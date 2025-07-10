2d69836f1e0ce903782b84b7abb6a3db9354c9e6
seungdeok <akk9132@naver.com>
2025-07-11 07:33:28 +0900

    refactor(api): 포트 타입 변환

--- apps/api/src/config/db.config.ts
+++ apps/api/src/config/db.config.ts
@@ -11,7 +11,7 @@ export default registerAs("database", () => {
   return {
     type: "mysql",
     host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
+    port: Number.parseInt(process.env.MYSQL_PORT ?? "3306"),
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
