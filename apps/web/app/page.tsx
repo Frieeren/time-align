8feea672b4d19c77db5e8965d5d55ea37b5bf5d3
seungdeok <akk9132@naver.com>
2025-09-08 23:41:27 +0900

    refactor(web): add base route

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,7 @@
-import { css } from "../styled-system/css";
-
-export default function Home() {
+export default function Page() {
   return (
-    <div
-      className={css({
-        bg: "blue.100",
-        minH: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      })}
-    >
-      <h1 className={css({ fontSize: "3xl", fontWeight: "body", color: "primary" })}>Hello, Panda CSS!</h1>
-    </div>
+    <section>
+      <div>Home</div>
+    </section>
   );
 }
--- apps/web/app/schedule/page.tsx
+++ apps/web/app/schedule/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <section>
-      <div>Profile</div>
+      <div>Schedule</div>
     </section>
   );
 }
