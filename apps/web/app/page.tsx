aadbce291cb20c17309b9e3dd273cef437d9cc0b
seungdeok <akk9132@naver.com>
2025-09-09 02:08:14 +0900

    refactor(web): add metadata

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,15 @@
+import type { Metadata } from "next";
 import { css } from "../styled-system/css";
 import { Calendar } from "./_components/Calendar";
 import { FAB } from "./_components/FAB";
 import { ScheduleCard } from "./_components/ScheduleCard";
 import { Tabs } from "./_components/Tabs";
 import { TopBanner } from "./_components/TopBanner";
+
+export const metadata: Metadata = {
+  title: "Time Align | 홈",
+  description: "일정관리의 시작, Time Align",
+};
 
 export default function Page() {
   return (
--- apps/web/app/schedule/page.tsx
+++ apps/web/app/schedule/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Time Align | 일정",
+  description: "일정관리의 시작, Time Align",
+};
+
 export default function Page() {
   return (
     <section>
