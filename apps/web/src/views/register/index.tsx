d7022e474dede5f37a8748fbfd8181d3efb02f57
seungdeok <akk9132@naver.com>
2025-10-12 13:43:16 +0900

    feat(web): 회원가입  페이지 TextField

---
+++ apps/web/src/views/register/index.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export function RegisterPage() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <div>Register</div>
+    </div>
+  );
+}
