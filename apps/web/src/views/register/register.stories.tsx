2e8973ff83f263f374cb045fbe0e06b0a21c3b07
seungdeok <akk9132@naver.com>
2025-10-18 11:11:36 +0900

    feat(web): 로그인 - SNS_계정으로_로그인

--- apps/web/src/views/register/register.stories.tsx
+++ apps/web/src/views/register/register.stories.tsx
@@ -186,3 +186,41 @@ export const 아이디_찾기_비밀번호_찾기_회원가입: Story = {
     );
   },
 };
+
+export const SNS_계정으로_로그인: Story = {
+  args: {},
+  render: () => {
+    return (
+      <div style={{ width: "360px", display: "flex", flexDirection: "row", gap: "12px", alignItems: "center" }}>
+        <div
+          style={{
+            width: "100%",
+            height: "1px",
+            backgroundColor: "#D9D9D9",
+          }}
+        />
+        <div
+          style={{
+            color: "#A7A7A7",
+            fontSize: "12px",
+            fontStyle: "normal",
+            fontWeight: "500",
+            lineHeight: "140%",
+            letterSpacing: "-0.24px",
+            textAlign: "center",
+            minWidth: "fit-content",
+          }}
+        >
+          SNS 계정으로 로그인
+        </div>
+        <div
+          style={{
+            width: "100%",
+            height: "1px",
+            backgroundColor: "#D9D9D9",
+          }}
+        />
+      </div>
+    );
+  },
+};
